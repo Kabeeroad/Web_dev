a3d10062a3bdaa25b348b1658be373f7fb990298
Kabeer <kabeeroad07@gmail.com>
2023-12-01 06:06:13 +0000

    adding new files

--- Project-2/sccript.js
+++ Project-2/sccript.js
@@ -109,3 +109,22 @@ gsap.from("#page4 h1", {
     scrub: 3,
   },
 });
+
+
+
+
+    // Function to open the popup
+    function openPopup() {
+        
+        document.getElementById('popup').style.display = 'block';
+    }
+
+    // Function to close the popup
+    function closePopup() {
+     
+        document.getElementById('popup').style.display = 'none';
+    }
+
+    // Open the popup when the page loads (you can remove this if you want to trigger the popup with a button or link)
+    window.onload = openPopup;
+

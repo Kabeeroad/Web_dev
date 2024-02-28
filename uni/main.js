102f44c80a06d30e85873aeb617ce449800bd62f
Kabeer <kabeeroad07@gmail.com>
2024-02-28 16:13:41 +0000

    adding new task

---
+++ uni/main.js
@@ -0,0 +1,51 @@
+// const menuBtnBsit = document.getElementById("menu-dpt-bsit");
+const menuBtnBscs = document.getElementById("menu-dpt-bscs");
+const menuBtnBsse = document.getElementById("menu-dpt-bsse");
+const menuBtnBed = document.getElementById("menu-dpt-bed");
+const menuBtnBba = document.getElementById("menu-dpt-bba");
+
+// const itPage = document.getElementById("it-page");
+const csPage = document.getElementById("cs-page");
+const sePage = document.getElementById("se-page");
+const bedPage = document.getElementById("bed-page");
+const bbaPage = document.getElementById("bba-page");
+
+// menuBtnBsit.addEventListener("click", () => {
+//   if (csPage.classList.contains("active")) {
+//     console.log("Do nothing");
+//   } else {
+//     csPage.classList.toggle("active");
+//   }
+// });
+
+menuBtnBscs.addEventListener("click", () => {
+  if (csPage.classList.contains("active")) {
+    console.log("Do nothing");
+  } else {
+    csPage.classList.toggle("active");
+  }
+});
+
+// menuBtnBsse.addEventListener("click", () => {
+//   if (sePage.classList.contains("active")) {
+//     console.log("Do nothing");
+//   } else {
+//     sePage.classList.toggle("active");
+//   }
+// });
+
+// menuBtnBed.addEventListener("click", () => {
+//   if (bedPage.classList.contains("active")) {
+//     console.log("Do nothing");
+//   } else {
+//     bedPage.classList.toggle("active");
+//   }
+// });
+
+// menuBtnBba.addEventListener("click", () => {
+//   if (bbaPage.classList.contains("active")) {
+//     console.log("Do nothing");
+//   } else {
+//     bbaPage.classList.toggle("active");
+//   }
+// });
